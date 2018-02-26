268975e87f68fd801d3b85f1e4f340988669ef69
mono <monotom@gmail.com>
2018-02-26 18:06:29 +0100

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.10.1"
 
 set :application, 'slidevision_eurpeana'
 
-set :repo_url, 'ssh://git@github.com:formslider/formslider.eurpeana.src.git'
+set :repo_url, 'git@github.com:formslider/formslider.eurpeana.src.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
